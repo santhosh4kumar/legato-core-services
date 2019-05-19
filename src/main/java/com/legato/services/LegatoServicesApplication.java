db8a9569fc07a07163a0f97dfbf07d9eef3918db
santhosh4kumar <47595934+santhosh4kumar@users.noreply.github.com>
2019-05-19 19:49:55 +0530

    initial commit

---
+++ src/main/java/com/legato/services/LegatoServicesApplication.java
@@ -0,0 +1,13 @@
+package com.legato.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegatoServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegatoServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/legato/services/LegatoServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.legato.services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LegatoServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
